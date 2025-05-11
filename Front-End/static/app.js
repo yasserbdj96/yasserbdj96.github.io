5564ebeafcac8a2cd22dbbbc6d9b8ad9061763c8
Boudjada Yasser <by.root96@gmail.com>
2025-05-11 15:50:57 +0100

    Update app.js

--- Front-End/static/app.js
+++ Front-End/static/app.js
@@ -6,7 +6,7 @@ async function loadDataOnce() {
     if (appData) return appData;
 
     try {
-        const response = await fetch('https://yasserbdj96.pythonanywhere.com/data.json');
+        const response = await fetch('./data.json');
         appData = await response.json();
         return appData;
     } catch (error) {
